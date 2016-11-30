18e65318c01be315bef14b2618c6ce4d13670147
telewolfbot <telewolfbot@gmail.com>
2016-11-30 11:14:47 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {102117869,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -253,7 +253,17 @@ Persian: @IranSeed
 ]],
     help_text_realm = [[
 Realm Commands:
+		
+!info
+		
+!joke
 
+!time
+		
+!tr [Word]
+		
+!azan [City]		
+		
 !creategroup [name]
 Create a group
 
